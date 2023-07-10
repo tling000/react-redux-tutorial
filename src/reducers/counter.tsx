76ce2d3b912c4a28aad80eba016a856ff2ae1fd1
taiga <taiga.kimura@shelfy.jp>
2023-07-10 16:59:28 +0900

    メッセージをstateで管理するように変更

--- src/reducers/counter.tsx
+++ src/reducers/counter.tsx
@@ -1,30 +1,54 @@
 const initCount = 0
+const initMessage = '路地裏の猫'
+let newCount = 0
 
 const initialState = {
-    count: initCount
+    count: initCount,
+    message: initMessage,
+}
+
+const updateMessage = (newCount) => {
+    let newMessage = initMessage
+    if (newCount < 0) {
+        newMessage = 'マイナス猫'
+      } else if (newCount >= 1000) {
+        newMessage = 'GOD猫！！！！'
+      } else if (newCount >= 100) {
+        newMessage = '猫！！！！'
+      } else if (newCount >= 10) {
+        newMessage = '下積み猫'
+      }
+    return newMessage
 }
 
 const counter = (state = initialState, action: any) => {
     switch (action.type) {
         case 'INCREMENT':
+            newCount = state.count + 1
             return {
                 ...state,
-                count: state.count + 1
+                count: newCount,
+                message: updateMessage(newCount)
             }
         case 'DECREMENT':
+            newCount = state.count - 1
             return {
                 ...state,
-                count: state.count - 1
+                count: newCount,
+                message: updateMessage(newCount)
             }
         case 'DOUBLE':
+            newCount = state.count * 2
             return {
                 ...state,
-                count: state.count * 2
+                count: newCount,
+                message: updateMessage(newCount)
             }
         case 'RESET':
             return {
                 ...state,
-                count: initCount
+                count: initCount,
+                message: initMessage
             }
     }
 
